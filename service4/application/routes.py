a608bb60d2d22bf67ae7c8204b5d5f5f3c066c79
Wasim-Danyal <wasim.danyal1@gmail.com>
2020-08-24 20:35:59 +0000

    logic implemented

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -1,13 +1,20 @@
 from flask import render_template, redirect, url_for, Response, request
 from application import app
 import requests 
-
+import random
 #replication of service3 to ensure routes working before i implement logic
 
-def decider(countrycity):
-    c = countrycity
-    return c 
-
+def decider(countrycity):   
+    foods = {
+        "Toronto": ["Poutine", "Veal Sandwich"],
+        "Moscow": ["Bliny", "Kasha"],
+        "California": ["Cheeseburger", "Burrito"],
+        "Prague": ["Trdelník", "Chlebíčky"],
+        "Paris": ["Baguette", "French Cheese"],
+    }
+    
+    return random.choice(foods[countrycity])
+    
 @app.route('/')
 @app.route('/food', methods=['GET', 'POST'])
 def travelchoice():
