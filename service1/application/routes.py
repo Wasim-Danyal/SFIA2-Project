d7effb154501c820a8ff13da8a0ebe12f6a1c8ee
Wasim-Danyal <wasim.danyal1@gmail.com>
2020-08-27 16:00:55 +0000

    fix

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request
 from application import app, db
 import requests
 from application.models import Destination
-from sqlalchemy import desc
+from sqlalchemy import desc, func
 
 @app.route('/')
 @app.route('/home')
@@ -11,13 +11,13 @@ def home():
 
 @app.route('/generate', methods=['GET','POST'])
 def generate():
-	country = requests.get('http://34.105.204.122:5001/country')
-	city = requests.post('http://34.105.204.122:5002/city', data=country.text)
-	food = requests.post('http://34.105.204.122:5003/food', data=city.text)
+	country = requests.get('http://service2:5001/country')
+	city = requests.post('http://service3:5002/city', data=country.text)
+	food = requests.post('http://service4:5003/food', data=city.text)
 
 	db_info = Destination(country=country.text, city=city.text, food=food.text)
 	db.session.add(db_info)
 	db.session.commit()
-	recents=Destination.query.order_by(country.desc)
+	recents=Destination.query.order_by(id.desc)
 	
 	return render_template('generate.html', countryname = country.text, cityname = city.text, food = food.text, display = recents)
