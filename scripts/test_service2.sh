e30d1dd9e97083ca0236004ffe5d50a089d184e3
Wasim <wasim.danyal1@gmail.com>
2020-08-30 21:09:42 +0100

    attempt to fix scripts

--- scripts/test_service2.sh
+++ scripts/test_service2.sh
@@ -1,3 +1,3 @@
-cd /home/wasim_danyal1/SFIA2-Project/service2
+cd SFIA2-Project/service2
 pip3 install -r requirements.txt
 pytest --cov=application
--- scripts/test_service3.sh
+++ scripts/test_service3.sh
@@ -1,3 +1,3 @@
-cd /home/wasim_danyal1/SFIA2-Project/service3
+cd SFIA2-Project/service3
 pip3 install -r requirements.txt
 pytest --cov=application
