6dba597d7a787662e892f8f8d42c06ddfeea1f65
Wasim <wasim.danyal1@gmail.com>
2020-08-30 22:07:18 +0100

    removed some scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+if ! [ -d SFIA2 ]; then
+    git clone https://github.com/Wasim-Danyal/SFIA2-Project.git
+fi
+cd SFIA2-Project
 sudo apt-get update
 sudo apt install -y python3 python3-pip python3-venv
 python3 -m venv venv 
 source venv/bin/activate
+cd service1
 pwd
--- scripts/test_service4.sh
+++
@@ -1,3 +0,0 @@
-cd SFIA2-Project/service4
-pip3 install -r requirements.txt
-pytest --cov=application
