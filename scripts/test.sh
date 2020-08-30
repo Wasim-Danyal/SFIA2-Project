180f716eaae581c344c7c6349f6701df64916b4c
Wasim <wasim.danyal1@gmail.com>
2020-08-30 22:49:25 +0100

    jenkins venv

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,21 @@ sudo apt install -y python3 python3-pip python3-venv
 python3 -m venv venv 
 source venv/bin/activate
 cd service1
+pip3 install -r requirements.txt
 pytest --cov=appliation
+cd ..
+
+cd service2
+pip3 install -r requirements.txt
+pytest --cov=appliation
+cd ..
+
+cd service3
+pip3 install -r requirements.txt
+pytest --cov=appliation
+cd ..
+
+cd service4
+pip3 install -r requirements.txt
+pytest --cov=appliation
+cd ..
