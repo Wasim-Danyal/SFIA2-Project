4a62409b97aefd1e29b01475b7c5180e0a798f2f
Wasim <wasim.danyal1@gmail.com>
2020-08-30 22:12:40 +0100

    jenkins test scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,6 @@
+if ! [ -d SFIA2 ]; then
+    git clone https://github.com/Wasim-Danyal/SFIA2-Project.git
+fi
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt install -y python3 python3-pip
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,3 @@
-if ! [ -d SFIA2 ]; then
-    git clone https://github.com/Wasim-Danyal/SFIA2-Project.git
-fi
 cd SFIA2-Project
 sudo apt-get update
 sudo apt install -y python3 python3-pip python3-venv
