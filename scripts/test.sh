292f93624ef0546645de40ddafced5cfdfeae7fa
Wasim <wasim.danyal1@gmail.com>
2020-08-31 11:34:13 +0100

    remove venv

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 cd SFIA2-Project
 cd service1
-
 python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
@@ -9,22 +8,16 @@ python3 -m pytest --cov application --cov-report term-missing
 cd ..
 
 cd service2
-python3 -m venv venv
-. venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov application --cov-report term-missing
 cd ..
 
 cd service3
-python3 -m venv venv
-. venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov application --cov-report term-missing
 cd ..
 
 cd service4
-python3 -m venv venv
-. venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov application --cov-report term-missing
 cd ..
