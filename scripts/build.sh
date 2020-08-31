ae6ef646a144a1acf874c5a474771d41b43663a1
Wasim <wasim.danyal1@gmail.com>
2020-08-31 10:46:34 +0100

    more changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-if ! [ -d SFIA2-Project ]; then
-    git clone https://github.com/Wasim-Danyal/SFIA2-Project.git
-fi
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt install -y python3 python3-pip
@@ -12,4 +9,3 @@ pip install --user ansible
 ~/.local/bin/ansible --version
 ~/.local/bin/ansible-playbook -i inventory.cfg playbook.yaml
 docker-compose build
-pwd
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
+cd SFIA2-Project
+$ echo $PWD
+cd service1
 sudo apt-get update
 sudo apt install -y python3 python3-pip python3-venv
 python3 -m venv venv 
 source venv/bin/activate
-
-cd service1
 pip3 install -r requirements.txt
 python3 -m pytest --cov application
 cd ..
