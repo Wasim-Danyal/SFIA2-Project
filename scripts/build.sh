6871e2039c136a6b2a5e8c67cc3be8fc79a66c19
Wasim <wasim.danyal1@gmail.com>
2020-08-31 20:57:47 +0100

    docker restage

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,11 @@ if ! [ -d SFIA2-Project ]; then
 fi
 cd SFIA2-Project
 git pull
+export DATABASE_URI="${DATABASE_URI}"
+docker-compose down --rmi all
+docker-compose build
+docker login -u "${username}" -p "${password}"
+docker-compose push
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt install -y python3 python3-pip
@@ -13,8 +18,3 @@ echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 pip3 install --user ansible
 ansible-playbook -i inventory playbook.yaml
-export DATABASE_URI="${DATABASE_URI}"
-docker-compose down --rmi all
-docker-compose build
-docker login -u "${username}" -p "${password}"
-docker-compose push
