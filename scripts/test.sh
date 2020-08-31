e7a416c6fa6966445d71b5939c42665895107332
Wasim <wasim.danyal1@gmail.com>
2020-08-31 10:27:13 +0100

    python3 pytest

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,22 +3,23 @@ sudo apt-get update
 sudo apt install -y python3 python3-pip python3-venv
 python3 -m venv venv 
 source venv/bin/activate
+
 cd service1
 pip3 install -r requirements.txt
-pytest --cov=appliation
+python3 -m pytest --cov application
 cd ..
 
 cd service2
 pip3 install -r requirements.txt
-pytest --cov=appliation
+python3 -m pytest --cov application
 cd ..
 
 cd service3
 pip3 install -r requirements.txt
-pytest --cov=appliation
+python3 -m pytest --cov application
 cd ..
 
 cd service4
 pip3 install -r requirements.txt
-pytest --cov=appliation
+python3 -m pytest --cov application
 cd ..
