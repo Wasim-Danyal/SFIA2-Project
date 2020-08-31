f8b2323ef2a207acf8555c11b3c7e5c883d07a87
Wasim <wasim.danyal1@gmail.com>
2020-08-31 18:35:02 +0100

    script change

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,13 +7,12 @@ cd SFIA2-Project
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt install -y python3 python3-pip
-export DATABASE_URI="${DATABASE_URI}"
-docker-compose build
-docker login -u "${username}" -p "${password}"
-docker-compose push
 mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 pip install --user ansible
-~/.local/bin/ansible --version
-~/.local/bin/ansible-playbook -i inventory playbook.yaml
+ansible-playbook -i inventory playbook.yaml
+export DATABASE_URI="${DATABASE_URI}"
+docker-compose build
+docker login -u "${username}" -p "${password}"
+docker-compose push
