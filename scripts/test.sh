1ee193732da63b8496d9cd3164db4283b15a2483
Wasim <wasim.danyal1@gmail.com>
2020-08-31 11:25:13 +0100

    cov report

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,27 +1,30 @@
 #! /bin/bash
 cd SFIA2-Project
 cd service1
-sudo apt install -y python3 python3-pip
+
 python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov application 
+python3 -m pytest --cov application --cov-report term-missing
 cd ..
+
 cd service2
 python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov application 
+python3 -m pytest --cov application --cov-report term-missing
 cd ..
+
 cd service3
 python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov=application 
+python3 -m pytest --cov application --cov-report term-missing
 cd ..
+
 cd service4
 python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov=application 
+python3 -m pytest --cov application --cov-report term-missing
 cd ..
