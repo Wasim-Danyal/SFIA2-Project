00257379e6a0de91f02f06dcb8e8883e17baef86
Wasim <wasim.danyal1@gmail.com>
2020-08-31 17:48:12 +0100

    jenkins attempt

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo -su wasim-danyal1@sfia2
+ssh wasim_danyal1@sfia2
 if ! [ -d SFIA2-Project ]; then
     git clone https://github.com/Wasim-Danyal/SFIA2-Project.git
 fi
@@ -7,17 +7,13 @@ cd SFIA2-Project
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt install -y python3 python3-pip
+export DATABASE_URI="${DATABASE_URI}"
+docker-compose build
+docker login -u "${username}" -p "${password}"
+docker-compose push
 mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 pip install --user ansible
 ~/.local/bin/ansible --version
 ~/.local/bin/ansible-playbook -i inventory playbook.yaml
-export DATABASE_URI="${DATABASE_URI}"
-docker login -u "${username}" -p "${password}"
-docker-compose down --rmi all
-docker-compose build
-docker push wasimd/service1:latest
-docker push wasimd/service2:latest
-docker push wasimd/service3:latest
-docker push wasimd/service4:latest
