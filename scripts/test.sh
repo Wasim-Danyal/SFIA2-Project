dd6a522efa034cffcc736916c324f391f1420988
Wasim <wasim.danyal1@gmail.com>
2020-08-31 11:15:47 +0100

    venv as python3

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,26 +1,26 @@
 #! /bin/bash
 cd SFIA2-Project
 cd service1
-python -m venv venv
+python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov application 
 cd ..
 cd service2
-python -m venv venv
+python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov application 
 cd ..
 cd service3
-python -m venv venv
+python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov=application 
 cd ..
 cd service4
-python -m venv venv
+python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
-pytest --cov=application 
+python3 -m pytest --cov=application 
 cd ..
